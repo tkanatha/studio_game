b3609a188e7df0d708916678711166b125bf9d46
Austin David <adavidphx@gmail.com>
2013-12-16 07:05:12 -0500

    Format player info.  Add fourth player

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Studio Game
+# Pragmatic Studio Game
 
 An text-based game written in Ruby that is based on The Pragmatic Studio's online introductory course "Ruby Programming" by Mike and Nicole Clark.  This is the exercise app that was given to students as a challenge to test their knowledge of the concepts taught in the course.
--- studio_game.rb
+++ studio_game.rb
@@ -1,9 +1,32 @@
 player1 = "larry"
 health1 = 60
-
 player2 = "curly"
+health2 = 125
+
+current_time = Time.new
+formatted_time = current_time.strftime("%A %m/%d/%y at %I:%M%p")
+
+puts "#{player1.capitalize} has a health of #{health1}."
+puts "#{player2.upcase} has a health of #{health2}."
+
+health2 = health1
+
+puts "#{player1.capitalize} has a health of #{health1}."
+puts "#{player2.upcase} has a health of #{health2}."
+
+health1 = 30
+
+puts "#{player1.capitalize} has a health of #{health1}."
+puts "#{player2.upcase} has a health of #{health2}."
+
 player3 = "moe"
+health3 = 100
 
-puts "Players:\n\t#{player1}\n\t#{player2}\n\t#{player3}"
+puts "#{player3.capitalize} has a health of #{health3}".center(50, '*')
 
-puts "#{player1}'s health is #{health1}"
+player4 = "shemp"
+health4 = 90
+
+puts "#{player4.capitalize.ljust(20,'.')} #{health4} health"
+
+puts "\nThe game started at #{formatted_time}"
