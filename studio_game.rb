a9b7479f5f7f03efabca4a02e16aee6e0346a274
Austin David <adavidphx@gmail.com>
2013-12-18 19:47:59 -0500

    Add virtual score attribute

--- studio_game.rb
+++ studio_game.rb
@@ -1,11 +1,18 @@
 class Player
+  attr_reader :health
+  attr_accessor :name
+
   def initialize(name, health=100)
     @name = name.capitalize
     @health = health
   end
 
+  def name=(name_change)
+    @name = name_change.capitalize
+  end
+
   def to_s
-    "#{@name} has a health of #{@health}."
+    "#{@name} has a health of #{@health} and a score of #{score}."
   end
 
   def heal
@@ -16,6 +23,10 @@ class Player
   def smash
     @health -= 5
     puts "#{@name} got smashed!"
+  end
+
+  def score
+    @health + @name.length
   end
 end
 
@@ -32,7 +43,6 @@ sally.heal
 puts sally
 sally.smash
 puts sally
-
 
 harriet = Player.new("harriet")
 puts harriet
